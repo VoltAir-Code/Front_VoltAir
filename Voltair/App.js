2be9b51998b5bd465ebb54c1af8b020ab0b104b9
Richard Fernandes Passarelli <45811737874@info.local>
2024-05-28 10:07:42 -0300

    Subindo navegacao

--- Voltair/App.js
+++ Voltair/App.js
@@ -1,20 +1,57 @@
 import { useState, useEffect, useRef } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//importando navegacoes
 
 //importando fonts
+import { useFonts } from 'expo-font';
 import{
-  useFonts,
   Poppins_400Regular,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
+import { Navigation } from "./src/screens/Navigation/Navigation";
+import { Login } from "./src/screens/Login/Login";
+import { StatusBar } from "react-native";
+
+//instancia do StackNavigator
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   
+  const[fontsLoaded, fontsError] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  })
+
+  if(!fontsLoaded && !fontsError){
+    return null;
+  }
+
   return(
-    <>
-    </>
+    <NavigationContainer>
+
+    <Stack.Navigator
+        screenOptions={{headerShown: false}}
+      >
+
+      <Stack.Screen
+        name='Navigation'
+        component={Navigation}
+        options={{title: 'Navigation'}}
+      />
+
+      <Stack.Screen
+        name='Login'
+        component={Login}
+        options={{title: 'Login'}}
+      />
+
+      </Stack.Navigator>
+
+<StatusBar
+      style="auto" 
+      />
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  
-});
---
+++ Voltair/src/screens/Login/Login.js
@@ -0,0 +1,6 @@
+export const Login = ({navigation}) => {
+    return(
+        <>
+        </>
+    )
+}
