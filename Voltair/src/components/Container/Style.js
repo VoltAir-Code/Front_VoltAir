b61709c00dc9332e7815296963c00da8d26fadff
Richard Fernandes Passarelli <45811737874@info.local>
2024-05-28 08:37:09 -0300

    Excluido arquivo de texto

--- Voltair/src/components/Container/Style.js
+++ Voltair/src/components/Container/Style.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
-export const Container = styled.SafeAreaView`
+export const ContainerBlack = styled.SafeAreaView`
     flex: 1;
     align-items: center;
-    background-color: #fafafa;
+    background-color: #313131;
+`
+
+export const ContainerWhite = styled.SafeAreaView`
+    align-items: center;
+    background-color: #FFFFFF;
+    height: ${props => `${props.height}`};
 `
