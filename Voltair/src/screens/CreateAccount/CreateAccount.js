a7c8c779f6b65fda29ae58f2b7293785b4c67d93
Gabriel Victor Souza Almeida <47349403801@info.local>
2024-05-29 08:16:46 -0300

    CreateAccount

--- Voltair/src/screens/CreateAccount/CreateAccount.js
+++ Voltair/src/screens/CreateAccount/CreateAccount.js
@@ -2,14 +2,14 @@ import { Image } from "react-native"
 import { ButtonDefault } from "../../components/Button/Button"
 import { ContainerBlack, ContainerWhite } from "../../components/Container/Style"
 import { InputWhite } from "../../components/Input/Style"
-import {TextLink} from "../../components/Title/Style"
+import { TextLink } from "../../components/Title/Style"
 
 
 export const CreateAccount = () => {
     return (
         <>
             <ContainerBlack>
-            
+
                 <ContainerWhite height={"91.41%"}>
                     <InputWhite height={"53px"} margin={"39px 0px 0px 0px"} placeholder={"digite seu nome"} />
                     <InputWhite height={"53px"} margin={"39px 0px 0px 0px"} placeholder={"digite seu melhor e-mail"} />
