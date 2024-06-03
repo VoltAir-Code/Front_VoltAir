21b536696148310909822d2db897081440e92de3
Artur Fiorentino <49957976885@info.local>
2024-06-03 10:53:26 -0300

    método cadastro finalizado

--- Voltair/src/screens/CreateAccount/CreateAccount.js
+++ Voltair/src/screens/CreateAccount/CreateAccount.js
@@ -6,25 +6,55 @@ import { LogoRayOrange } from "../../components/Logo/Style"
 import ModalDefault from "../../components/Modal/ModalDefault"
 import { TextLink } from "../../components/Title/Style"
 import { useState } from "react"
+import api from "../../services/Service"
+import { err } from "react-native-svg"
 
 export const CreateAccount = ({ navigation }) => {
 
     const [modalVisible, setModalVisible] = useState(false);
+const[nome, setNome] = useState();
+const[email,setEmail] = useState();
+const[senha, setSenha] = useState();
+const[confirmeSenha, setConfimeSenha] = useState();
+    async function Register(){
+if(senha != null && confirmeSenha == senha && senha.length > 3)
+{
 
+    try {
+
+        const response = await api.post('Usuario', {
+            nome: nome,
+            email: email,
+            senha: senha,
+        
+          }) 
+          setModalVisible(true);  
+    } catch (error) {
+        console.log(error);
+    }
+
+
+
+
+
+}else{
+    alert("Senha inválida")
+}
+    }
     return (
         <>
             <ContainerBlack>
                 <LogoRayOrange margin={"0px 0px 4px 0px"} source={require("../../../assets/Logo/LogoRay.png")}/>
                 <ContainerWhite height={"91.41%"}>
-                    <InputWhite height={"53px"} margin={"39px 0px 0px 0px"} placeholder={"digite seu nome"} />
-                    <InputWhite height={"53px"} margin={"39px 0px 0px 0px"} placeholder={"digite seu e-mail"} />
-                    <InputWhite height={"53px"} margin={"39px 0px 0px 0px"} placeholder={"digite sua senha"} />
-                    <InputWhite height={"53px"} margin={"39px 0px 108px 0px"} placeholder={"confirme sua senha"} />
+                    <InputWhite value={nome} onChangeText={setNome} height={"53px"} margin={"39px 0px 0px 0px"} placeholder={"digite seu nome"} />
+                    <InputWhite value={email} onChangeText={setEmail} height={"53px"} margin={"39px 0px 0px 0px"} placeholder={"digite seu e-mail"} />
+                    <InputWhite value={senha} onChangeText={setSenha} height={"53px"} margin={"39px 0px 0px 0px"} placeholder={"digite sua senha"} />
+                    <InputWhite value={confirmeSenha} onChangeText={setConfimeSenha} height={"53px"} margin={"39px 0px 108px 0px"} placeholder={"confirme sua senha"} />
                     <ButtonDefault
                         text={"Cadastrar"}
                         height={"58px"}
                         margin={"0px 0px 0px 0px"}
-                        onPress={() => setModalVisible(true)}
+                        onPress={() => Register()}
                     />
 
                     <TouchableOpacity>
