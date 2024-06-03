ef99e684888b28a44a635accf7dc2728e45568ff
Anna Beatriz Barbosa Cheliga Horta <52161105850@info.local>
2024-06-03 09:12:02 -0300

    Subindo alterações na home

--- Voltair/src/components/Card/Card.js
+++ Voltair/src/components/Card/Card.js
@@ -1,33 +1,33 @@
 import { StyleSheet, Text } from "react-native"
-import { CardImagens, CardInformation, ContainerCard, ImageCard} from "./Style"
+import { CardImagens, CardInformation, ContainerCard, ImageCard } from "./Style"
 import { useState } from "react";
 import ModalLoading from "../Modal/ModalLoading";
 import ModalInformations from "../Modal/ModalInformations";
 
-export const Card = ({navigation}) => {
+export const Card = ({ navigation }) => {
 
-    const [modalLoadingVisible, setModalLoadingVisible] = useState(false);    
-    const [modalInformationsVisible, setModalInformationsVisible] = useState(false);    
-    
+    const [modalLoadingVisible, setModalLoadingVisible] = useState(false);
+    const [modalInformationsVisible, setModalInformationsVisible] = useState(false);
 
-return(
-    <ContainerCard>
-        <CardImagens onPress={() => setModalLoadingVisible(true)} >
-        <ImageCard source={require("../../../assets/Imagem-home-01.png")}/>
-        <CardInformation>
-            <Text style={styles.textCard}>Carregamento</Text>
-        </CardInformation>
-        </CardImagens>
-        
-        <CardImagens  onPress={() => setModalInformationsVisible(true)}>
-        <ImageCard source={require("../../../assets/Imagem-home-02.png")}/>
-        <CardInformation>
-        <Text style={styles.textCard}>Informações Da Bateria </Text>
-        </CardInformation>
-        </CardImagens>
-        
-        
-        <ModalLoading
+
+    return (
+        <ContainerCard>
+            <CardImagens onPress={() => setModalLoadingVisible(true)} >
+                <ImageCard source={require("../../../assets/Imagem-home-01.png")} />
+                <CardInformation>
+                    <Text style={styles.textCard}>Carregamento</Text>
+                </CardInformation>
+            </CardImagens>
+
+            <CardImagens onPress={() => setModalInformationsVisible(true)}>
+                <ImageCard source={require("../../../assets/Imagem-home-02.png")} />
+                <CardInformation>
+                    <Text style={styles.textCard}>Informações Da Bateria </Text>
+                </CardInformation>
+            </CardImagens>
+
+
+            <ModalLoading
                 visible={modalLoadingVisible}
                 navigation={navigation}
                 onClose={() => setModalLoadingVisible(false)}
@@ -43,28 +43,28 @@ return(
                 onClose={() => setModalInformationsVisible(false)}
                 setModalVisible={setModalInformationsVisible}
                 title={"Informações Da Bateria"}
-                 information1={"Ciclo de vida "}
-                 answer1={"98% de capacidade restante"}
-                 information2={"Degradação"}
-                 answer2={'Capacidade da Bateria: 75 kWh Eficiência do Veículo: 0,15 kWh/km'}
-                 information3={'Autonomia'}
-                 answer3={'500km'}
-                 buttonTextLoading={"teste"}
+                information1={"Ciclo de vida "}
+                answer1={"98% de capacidade restante"}
+                information2={"Degradação"}
+                answer2={'Capacidade da Bateria: 75 kWh Eficiência do Veículo: 0,15 kWh/km'}
+                information3={'Autonomia'}
+                answer3={'500km'}
+                buttonTextLoading={"teste"}
                 buttonText={"Confirmar"}
             />
 
-    </ContainerCard>
+        </ContainerCard>
 
-    
-)
+
+    )
 }
 
 const styles = StyleSheet.create({
-    textCard:{
-       fontSize:16,
-       color:'#313131',
-       fontFamily: 'Poppins_400Regular',
-       textAlign: 'center',
-       alignSelf: 'center'
+    textCard: {
+        fontSize: 16,
+        color: '#313131',
+        fontFamily: 'Poppins_400Regular',
+        textAlign: 'center',
+        alignSelf: 'center'
     }
-    })
+})
