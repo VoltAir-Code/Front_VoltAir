d8d2690d2f05afcbbaab7eb3690f18713963146e
Gabriel Victor Souza Almeida <47349403801@info.local>
2024-06-03 08:42:30 -0300

    Merge Gabriel MapScreen

--- Voltair/src/components/MapFooter/MapFooter.js
+++ Voltair/src/components/MapFooter/MapFooter.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import { ButtonDefaultCircle } from "../Button/Button"
 import { ContainerBlackMap } from "../Container/Style"
+import { Ionicons } from '@expo/vector-icons';
 
 export const MapFooter = () => {
-    return(
+    const [run, setRun] = useState(false)
+    return (
         <ContainerBlackMap flexDirection={"row"} justifyContent={"center"}>
             <ButtonDefaultCircle
                 text={"Iniciar corrida"}
-                source={require("../../../assets/Img/Play.png")}
+                icon={
+                    run ?
+                        <Ionicons name="stop" size={54} color="black" />
+                        :
+                        <Ionicons name="play" size={54} color="black" style={{ alignSelf: "center", marginLeft: 7 }} />
+                }
+                onPress={() => { run ? setRun(false) : setRun(true) }}
             />
         </ContainerBlackMap>
     )
--- Voltair/src/components/MapHeader/MapHeader.js
+++ Voltair/src/components/MapHeader/MapHeader.js
@@ -1,14 +1,21 @@
-import { Image } from "react-native"
+import { Image, TouchableOpacity } from "react-native"
 import { ContainerTransparentMap } from "../Container/Style"
 import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import { useState } from "react";
 
-export const MapHeader = () => {
+
+export const MapHeader = ({navigation}) => {
+    const [run, setRun] = useState(false)
+
     return (
         <>
             <ContainerTransparentMap>
 
-                <AntDesign name="left" size={30} color="black" />
+                <TouchableOpacity>
+                    <AntDesign name="left" size={30} color="black" />
+                </TouchableOpacity>
+
                 <Ionicons name="notifications-outline" size={30} color="black" />
 
             </ContainerTransparentMap>
