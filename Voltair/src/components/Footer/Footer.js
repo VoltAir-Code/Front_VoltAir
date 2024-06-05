81ecece998c131763a563b15d998a6764badc86a
Gabriel Victor Souza Almeida <47349403801@info.local>
2024-06-05 07:56:55 -0300

    Git merge Gabriel

--- Voltair/src/components/Footer/Footer.js
+++ Voltair/src/components/Footer/Footer.js
@@ -1,42 +1,42 @@
-import { ButtonHome,ButtonProfile, CircleButton, ImageCar, ImageCircle, ImageMap, ImageRay,  } from "../Button/Button"
+import { ButtonHome, ButtonProfile, CircleButton, ImageCar, ImageCircle, ImageMap, ImageRay, } from "../Button/Button"
 import { ContainerBlackHome } from "../Container/Style"
 import { StyleSheet, Text } from "react-native"
 
 
 
-export const Footer = ()=>{
-return(
-   <ContainerBlackHome height={"15%"} radius={'10px 10px 0px 0px'} gap={'80px'}>
-      <ButtonHome>
-      <ImageRay source={require("../../../assets/Logo/LogoRay.png")}/>
-      <Text style={styles.textFooter}>Home</Text>
-      </ButtonHome>
+export const Footer = () => {
+   return (
+      <ContainerBlackHome height={"15%"} radius={'10px 10px 0px 0px'} gap={'80px'}>
+         <ButtonHome>
+            <ImageRay source={require("../../../assets/Logo/LogoRay.png")} />
+            <Text style={styles.textFooter}>Home</Text>
+         </ButtonHome>
 
-      <CircleButton>
-      <ImageCircle source={require("../../../assets/Img/MapPoint.png")}/>
-      <Text style={styles.textFooter}>Mapa</Text>
-      </CircleButton>
+         <CircleButton>
+            <ImageCircle source={require("../../../assets/Img/MapPoint.png")} />
+            <Text style={styles.textFooter}>Mapa</Text>
+         </CircleButton>
 
-      <ButtonProfile>
-      <ImageCar source={require("../../../assets/Img/Volante.png")}/>
-      <Text style={styles.textFooter}>Meu carro</Text>
-      </ButtonProfile>
-      
-   </ContainerBlackHome>
-   
-)
+         <ButtonProfile>
+            <ImageCar source={require("../../../assets/Img/Volante.png")} />
+            <Text style={styles.textFooter}>Meu carro</Text>
+         </ButtonProfile>
+
+      </ContainerBlackHome>
+
+   )
 }
 
 const styles = StyleSheet.create({
-textFooter:{
-   fontSize:16,
-   color: 'white',
-   fontFamily: 'Poppins_400Regular',
-   textAlign: 'center',
-   alignSelf: 'center',
-   marginTop: 20,
-   height:40,
-   width:100,
-   marginRight:'3%'
-}
+   textFooter: {
+      fontSize: 16,
+      color: 'white',
+      fontFamily: 'Poppins_400Regular',
+      textAlign: 'center',
+      alignSelf: 'center',
+      marginTop: 20,
+      height: 40,
+      width: 100,
+      marginRight: '3%'
+   }
 })
