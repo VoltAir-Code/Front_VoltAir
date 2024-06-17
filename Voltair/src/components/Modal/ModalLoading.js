c66bd7d05151ba6f5b7eef398f0f344a3b828847
Richard Fernandes Passarelli <45811737874@info.local>
2024-06-17 09:50:11 -0300

    Subindo botao de parar carregamento

--- Voltair/src/components/Modal/ModalLoading.js
+++ Voltair/src/components/Modal/ModalLoading.js
@@ -1,7 +1,7 @@
 import { Alert, Modal } from "react-native";
-import { ButtonDefault, ButtonLoading, ImageBatery } from "../Button/Button"
-import { SubTitle, TextLink, Title } from "../Title/Style"
-import { BatteryPercentage, ModalContainer } from "./Style"
+import { ButtonDefault, ButtonLoading, ImageBatery } from "../Button/Button";
+import { SubTitle, TextLink, Title } from "../Title/Style";
+import { BatteryPercentage, ModalContainer } from "./Style";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useDecodeToken } from "../../utils/Auth";
 import api from "../../services/Service";
@@ -21,19 +21,20 @@ const ModalLoading = ({
     buttonTextLoading,
     charging,
 }) => {
-    const [timer, setTimer] = useState(false)
-    const [car, setCar] = useState({})
+    const [timer, setTimer] = useState(false);
+    const [intervalId, setIntervalId] = useState(null);
+    const [car, setCar] = useState({});
     const [idUser, setIdUser] = useState();
-    const [battery, setBattery] = useState()
-    const batteryCapacity = car.capacidade
+    const [battery, setBattery] = useState();
+    const batteryCapacity = car.capacidade;
     const [progressValue, setProgressValue] = useState(0);
     const [notificationScheduled, setNotificationScheduled] = useState(false);
-    const [newBattery, setNewBattery] = useState(battery)
+    const [newBattery, setNewBattery] = useState(battery);
 
     useEffect(() => {
         InformationCar();
-        LoadDataUser()
-    }, [])
+        LoadDataUser();
+    }, []);
 
     useEffect(() => {
         setBattery(progressValue);
@@ -41,59 +42,54 @@ const ModalLoading = ({
             setNotificationScheduled(true);
             scheduleNotification();
         }
-
     }, [progressValue]);
-
 
     useEffect(() => {
         UpdateTime();
-    }, [newBattery])
-
+    }, [newBattery]);
 
     const UpdateTime = async (newValue) => {
         try {
             const response = await api.put(`Carro/AtualizarBateria?idUsuario=${idUser}`, {
-                bateriaAtual: newBattery
-            })
+                bateriaAtual: newBattery,
+            });
         } catch (error) {
             console.log(error);
         }
-    }
-
+    };
 
     const LoadDataUser = async () => {
         const token = await useDecodeToken();
-
         const response = await api.get(`Carro/BuscarPorId?idUser=${token.id}`);
-        setIdUser(token.id)
-    }
-
+        setIdUser(token.id);
+    };
 
     async function LoadingCar() {
-        const intervalCharging = setInterval(() => {
+        const id = setInterval(() => {
             setProgressValue(prev => {
                 const newValue = Math.min(1, prev + (1 / batteryCapacity));
                 if (newValue >= 1) {
-                    clearInterval(intervalCharging);
+                    clearInterval(id);
                     setTimer(false);
-                }   
+                    setIntervalId(null);
+                }
                 setNewBattery(newValue);
                 return newValue;
             });
         }, 1000);
+        setIntervalId(id);
     }
 
     async function InformationCar() {
         const user = await useDecodeToken();
-
         api.get(`Carro/BuscarPorId?idUser=${user.id}`)
             .then(response => {
                 console.log(response.data);
-                setCar(response.data.idModeloNavigation)
-                setBattery(response.data.bateriaAtual)
+                setCar(response.data.idModeloNavigation);
+                setBattery(response.data.bateriaAtual);
             }).catch(err => {
-
-            })
+                console.log(err);
+            });
     }
 
     const scheduleNotification = async () => {
@@ -118,10 +114,21 @@ const ModalLoading = ({
                 </BatteryPercentage>
 
                 <ButtonLoading
-                    text={timer ? LoadingCar() : `${buttonTextLoading}`}
+                    text={timer ? "Carregando..." : `${buttonTextLoading}`}
                     height={"58px"}
                     margin={"10px 0px 10px 0px"}
-                    onPress={() => { timer ? setTimer(false) : setTimer(true) }}
+                    onPress={() => {
+                        if (!timer) {
+                            setTimer(true);
+                            LoadingCar();
+                        } else {
+                            setTimer(false);
+                            if (intervalId) {
+                                clearInterval(intervalId);
+                                setIntervalId(null);
+                            }
+                        }
+                    }}
                 />
 
                 <ButtonDefault
@@ -136,7 +143,8 @@ const ModalLoading = ({
                 </TextLink>
             </ModalContainer>
         </Modal>
-    )
+    );
 }
 
 export default ModalLoading;
+
